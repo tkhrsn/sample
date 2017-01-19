4957b1126098d36f8b413334ccde15f75b483c9f
Sano Takahiro <tkhrsn@yahoo.co.jp>
2017-01-19 14:33:55 +0900

    7.1.2 Userを表示する画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -7,4 +6,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+
+  resources :users
 end
